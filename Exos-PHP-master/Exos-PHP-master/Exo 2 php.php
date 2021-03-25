c9bf0243108253035aea01781378769788d90c44
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-25 15:57:40 +0100

    Test

---
+++ Exos-PHP-master/Exos-PHP-master/Exo 2 php.php
@@ -0,0 +1,7 @@
+<?php
+$i =0;
+do{
+    echo 'Je dois apprendre mon cours par coeur', '<br>', "\n";
+    $i = $i + 1;
+}
+while($i!==100);
---
+++ Exos-PHP-master/Exos-PHP-master/Exos 3.php
@@ -0,0 +1,10 @@
+<?php
+for($i=0;$i<=28;$i+=1){   
+
+    $result = $f*$opé;
+}
+echo $result;
+?>
+
+/*correction
+for($f = 1, )
