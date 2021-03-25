a62fb7a97d473cfabf2fc4a6afaf7486f48884a3
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-25 16:24:23 +0100

    Exo 4

---
+++ Exos-PHP-master/Exos-PHP-master/Exo 3.php
@@ -0,0 +1,15 @@
+<?php
+for($i=0;$i<=28;$i+=1){   
+
+    $result = $f*$opé;
+}
+echo $result;
+
+?>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
