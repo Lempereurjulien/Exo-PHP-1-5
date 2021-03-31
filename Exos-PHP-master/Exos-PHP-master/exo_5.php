0caee01b78e45420e7c4b1fc42b094c0a3c0d6cd
Julien LEMPEREUR <julien.lempereur@lyc-bonaparte.fr>
2021-03-31 14:41:18 +0200

    Rajout case dans exercice 5

--- Exos-PHP-master/Exos-PHP-master/exo_5.php
+++ Exos-PHP-master/Exos-PHP-master/exo_5.php
@@ -1,16 +1,25 @@
-<?php
-for($j=1; $j<=12; $j++){
-    echo "Table : ",$j,"<br>";
-    echo "<br>";
-for($i=0;$i<=10;$i++){   
+<head>
+    <link href="Css_5.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <table>
 
-    echo $i*$j , "<br>";
-}
-echo "<br>";
-}
-?>
-
-
-
-
+        <?php
+        for ($j = 1; $j <= 12; $j++) {
+            ?>
+        <th><td><?php echo $j ?></td></th>
+            <tr>
+                <?php
+                for ($i = 1; $i <= 10; $i++) {
+                    $totale = $i * $j;
+                    ?>
+                <td><?php echo $totale ?></td>
+                </tr>
+                <?php
+                
+            }
+        }
+        ?>
+    </table>
+</<body>
 
